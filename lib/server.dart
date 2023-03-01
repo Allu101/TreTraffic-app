05544335916e9097fcab6e7982255c31c7228879
Allu101 <a.lahtinen101@gmail.com>
2023-03-01 18:44:43 +0200

    Replace triggerpoint with triggerarea on itsenäisyydenkatu and update its routename

--- lib/server.dart
+++ lib/server.dart
@@ -21,7 +21,7 @@ class Server {
 
   //LatLong, routeName
   Map<LatLng, String> triggerPoints = {
-    LatLng(61.498947, 23.784609): 'sammonaukio:tamk',
+    LatLng(61.498947, 23.784609): 'itsenäisyydenkatu:teiskontie',
     LatLng(61.503598, 23.816772): 'tamk:sammonaukio',
     LatLng(61.490728, 23.812822): 'vuohenoja:ita',
     LatLng(61.486406, 23.829730): 'vuohenoja:lansi',
@@ -29,21 +29,24 @@ class Server {
     LatLng(61.480922, 23.872790): 'vt9N:sammonvaltatie',
   };
 
-  //Polygon coordinates, routeName
+  //Polygon coordinates (counter clockwise order), routeName
   Map<List<mapTools.LatLng>, String> triggerAreas = {
     [mapTools.LatLng(61.497292, 23.881377), mapTools.LatLng(61.497175, 23.881034),
-      mapTools.LatLng(61.497799, 23.879403), mapTools.LatLng(61.498027, 23.879886)]: 'linnainmaa:teiskontie',
+      mapTools.LatLng(61.497799, 23.879403), mapTools.LatLng(61.498015, 23.879873)]: 'linnainmaa:teiskontie',
+
+    [mapTools.LatLng(61.498869, 23.782754), mapTools.LatLng(61.498859, 23.780938),
+      mapTools.LatLng(61.498978, 23.780947), mapTools.LatLng(61.498987, 23.782747)]: 'itsenäisyydenkatu:teiskontie',
   };
 
   //Routename, [numero;suunta:suunta]
   Map<String, List<String>> routes = {
+    'itsenäisyydenkatu:teiskontie': ['428;1:2', '401;1:2', '424;1:2'],
     'linnainmaa:teiskontie': ['608;3'],
-    'sammonaukio:tamk': ['401;1:2', '424;1:2'],
     'tamk:sammonaukio': ['601;3:4'],
-    'vuohenoja:ita': ['509;1'],
-    'vuohenoja:lansi': ['509;3'],
     'vt9S:messukylankatu': ['663;3'],
     'vt9N:sammonvaltatie': ['666;3'],
+    'vuohenoja:ita': ['509;1'],
+    'vuohenoja:lansi': ['509;3'],
   };
 
   List<String> getAllDirectionsFromIntersection(String intersectionNro) {
